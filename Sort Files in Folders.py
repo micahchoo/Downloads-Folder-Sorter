c85c63e9664c39d9260e1d95e531a1b230aace38
Micah Alex <47268348+micahchoo@users.noreply.github.com>
2023-03-20 19:01:22 -0700

    Update Sort Files in Folders.py
    Here I start seeing, how this could become interesting for me. Also, this versioning was interesting. Around the time that I saved this one, I noticed how I was freezing v3 when I was ready with v4.

--- Sort Files in Folders.py
+++ Sort Files in Folders.py
@@ -1,131 +1,41 @@
 import os
 import shutil
-import datetime
+from datetime import datetime, timedelta
 
+# Define the path to the directory to organize
+dir_path = "C:/Users/micah/Downloads/_Seminar 03 - Documentation book/"
 
-# Path of the directory to be organized - i used this as a test bed
-path = "C:/Users/micah/Downloads/_Seminar 03 - Documentation book/"
-
-# Date range for files to be moved to Current folder
-current_date = datetime.datetime.now()
-three_months_ago = current_date - datetime.timedelta(days=90)
-
-
-
-# List of extensions and their corresponding folder names
-extensions = {
-    '.png': 'Images',
-    '.jpg': 'Images',
-    '.jpeg': 'Images',
-    '.gif': 'Images',
-    '.bmp': 'Images',
-    '.tiff': 'Images',
-    '.eps': 'Images',
-    '.raw': 'Images',
-    '.cr2': 'Images',
-    '.nef': 'Images',
-    '.orf': 'Images',
-    '.sr2': 'Images',
-    '.txt': 'Text Files',
-    '.doc': 'Text Files',
-    '.docx': 'Text Files',
-    '.log': 'Text Files',
-    '.ppt': 'Presentation',
-    '.pptx': 'Presentation',
-    '.csv': 'Data Files',
-    '.key': 'Data Files',
-    '.keychain': 'Data Files',
-    '.dat': 'Data Files',
-    '.sdf': 'Data Files',
-    '.tar': 'Data Files',
-    '.xml': 'Data Files',
-    '.vcf': 'Data Files',
-    '.aif': 'Music',
-    '.iff': 'Music',
-    '.m3u': 'Music',
-    '.m4a': 'Music',
-    '.mid': 'Music',
-    '.mp3': 'Music',
-    '.mpa': 'Music',
-    '.wav': 'Music',
-    '.wma': 'Music',
-    '.3g2': 'Videos',
-    '.3gp': 'Videos',
-    '.asf': 'Videos',
-    '.avi': 'Videos',
-    '.flv': 'Videos',
-    '.m4v': 'Videos',
-    '.mov': 'Videos',
-    '.mp4': 'Videos',
-    '.mpg': 'Videos',
-    '.rm': 'Videos',
-    '.zip': 'Compressed Files',
-    '.rar': 'Compressed Files',
-    '.7z': 'Compressed Files',
-    '.bz2': 'Compressed Files',
-    '.gz': 'Compressed Files',
-    '.tar.gz': 'Compressed Files',
-    '.dmg': 'Disk Image Files',
-    '.iso': 'Disk Image Files',
-    '.exe': 'Executables',
-    '.jar': 'Executables',
-    '.msi': 'Executables',
-    '.deb': 'Executables',
-    '.dll': 'System Files',
-    '.sys': 'System Files',
-    '.cfg': 'Settings Files',
-    '.ini': 'Settings Files',
-    '.json': 'Settings Files',
-    '.plist': 'Settings Files',
-    '.bak': 'Misc Files',
-    '.tmp': 'Misc Files',
-    '.crdownload': 'Misc Files',
-    '.part': 'Misc Files',
-    '': 'Misc Files',
-    '.apk': 'Android App Package',
-    '.bib': 'Data Files',
-    '.html': 'Web Files',
-    '.css': 'Web Files',
-    '.fig': '3D',
-    '.folder': 'System Files',
-    '.flatpakref': 'System Files',
-    '.webp': 'Images',
-    '.js': 'Web Files',
-    '.json': 'Data Files',
-    '.kdbx': 'Database',
-    '.md': 'Text Files',
-    '.opml': 'Text Files',
-    '.py': 'Developer Files',
-    '.svg': 'Images',
-    '.tid': 'Data Files',
-    '.vcf': 'Data Files',
-    '.xml': 'Data Files',
-    '.xpi': 'System Files'
+# Define the names of the folders to create and their associated file extensions
+folders = {
+    "Current": set(),
+    "Images": {".jpg", ".jpeg", ".png", ".gif", ".bmp"},
+    "Documents": {".pdf", ".doc", ".docx", ".txt", ".rtf", ".odt"},
+    "Videos": {".mp4", ".avi", ".mov", ".wmv", ".flv", ".mkv"},
+    "Music": {".mp3", ".wav", ".flac", ".aac", ".m4a"},
+    "Archives": {".zip", ".rar", ".tar", ".gz"},
+    "Miscellaneous": set()
 }
-# Create Current folder if it doesn't exist
-current_folder = os.path.join(path, "Current")
-if not os.path.exists(current_folder):
-    os.makedirs(current_folder)
 
-# Create folders for the extensions
-for folder_name in extensions.values():
-    folder_path = os.path.join(path, folder_name)
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
+# Define the time threshold for moving files to the Current folder
+current_threshold = datetime.now() - timedelta(days=90)
 
-# Move files created within the last 3 months to Current folder
-for file_name in os.listdir(path):
-    file_path = os.path.join(path, file_name)
-    if os.path.isfile(file_path):
-        file_creation_time = datetime.datetime.fromtimestamp(os.path.getctime(file_path))
-        if file_creation_time >= three_months_ago:
-            shutil.move(file_path, os.path.join(current_folder, file_name))
-
-# Move files older than 3 months to their respective folders
-for file_name in os.listdir(path):
-    file_path = os.path.join(path, file_name)
-    if os.path.isfile(file_path):
-        file_ext = os.path.splitext(file_name)[1]
-        folder_name = extensions.get(file_ext, 'Misc Files')
-        folder_path = os.path.join(path, folder_name)
-        shutil.move(file_path, os.path.join(folder_path, file_name))
+# Organize the files
+for filename in os.listdir(dir_path):
+    filepath = os.path.join(dir_path, filename)
+    if os.path.isfile(filepath):
+        ext = os.path.splitext(filename)[1].lower()
+        if ext in folders["Current"]:
+            shutil.move(filepath, os.path.join(dir_path, "Current", os.path.basename(filename)))
+        elif ext in folders["Miscellaneous"]:
+            shutil.move(filepath, os.path.join(dir_path, "Miscellaneous", os.path.basename(filename)))
+        else:
+            found = False
+            for folder, exts in folders.items():
+                if folder == "Current" or folder == "Miscellaneous":
+                    continue
+                if ext in exts:
+                    shutil.move(filepath, os.path.join(dir_path, folder, os.path.basename(filename)))
+                    found = True
+                    break
+            if not found and os.path.getctime(filepath) >= current_threshold:
+                shutil.move(filepath, os.path.join(dir_path, "Current", os.path.basename(filename)))
