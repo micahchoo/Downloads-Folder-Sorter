045559a56d88da06543cdd376143bc248ccaa184
Micah Alex <47268348+micahchoo@users.noreply.github.com>
2023-03-20 20:46:20 -0700

    2:30 pm the next day
    i woke up, and spoke to S for three hours. I spoke to them of  all the weird twists and turns of this story. And they say, you should publish it. 
    
    Something is happening in my brain as I talk to them
    
    we speak of public spaces, internet as a public space.
    How loitering in a public space means claiming it as your own. They tell me about a group of women who gather to go loiter at night and do all the things that people tell them not to do. Like sleep outside at night. We talk about embodied Data and how we wield it as a tool, id card, something we care about, something we pay for, and something we pay with. If the internet is a human right/ What does it mean for the Internet to be a human right. I remember this article 
    https://www.indiatoday.in/india/story/kashmir-survived-without-4g-internet-for-months-with-coronavirus-it-really-needs-it-1659883-2020-03-26
    
    I tell them about my theory on what is going to happen if AI is the future. 
    
    "I think the general public will have very little reason to remain on the internet after sometime. The creator economy will be devastated. what happens the post-attention economy doesn't look too good. The only entities with any need to be on the internet will be these AI agents, A new paradigm but reminiscent of the platform paradigm of yesteryear. i see class systems for agents like bollywood tabloids calling random actors A-tier and B-tier. It is no long a public space, but a giant TV, ENDLESS HEDONY"

--- Sort Files in Folders.py
+++ Sort Files in Folders.py
@@ -1,3 +1,66 @@
--------------------------
-Silence Ensues
--------------------------
+import os
+import shutil
+from datetime import datetime, timedelta
+
+# Define the path to the directory to organize
+path = "C:/Users/micah/Downloads/_Seminar 03 - Documentation book/"
+
+# Define the names of the folders to create and their associated file extensions
+
+folders = {
+    "Current": set(),
+    "Documents": {".doc", ".docx", ".txt", ".md", ".opml", ".tex", ".bib"},
+    "Presentations": {".ppt", ".pptx", ".key", ".odp"},
+    "Spreadsheets": {".xls", ".xlsx", ".csv"},
+    "Images": {".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp", ".svg"},
+    "Vector Graphics": {".ai", ".eps"},
+    "pdfs": {".pdf"},
+    "Raster Graphics": {".psd", ".raw", ".cr2", ".nef", ".orf", ".sr2"},
+    "Video": {".avi", ".mp4", ".mov", ".wmv", ".mkv", ".flv", ".webm", ".mpg", ".mpeg", ".3gp"},
+    "Audio": {".mp3", ".wav", ".m4a", ".aac", ".ogg", ".flac", ".wma"},
+    "Code": {".py", ".ipynb", ".java", ".cpp", ".c", ".h", ".cs", ".xml", ".json", ".yaml", ".yml", ".sql", ".rb", ".pl", ".sh", ".bat", ".cmd", ".ps1", ".dockerfile", ".fig", ".js"},
+    "Web Files": {".html", ".css", ".js", ".php"},
+    "Database": {".sqlite", ".db", ".sql", ".kdbx"},
+    "Executable Files": {".exe", ".msi", ".deb", ".dmg", ".appimage", ".sh", ".apk", ".xpi"},
+    "System Files": {".ini", ".cfg", ".plist", ".log", ".env", ".desktop", ".folder", ".flatpakref"},
+    "Compressed Files": {".zip", ".tar", ".gz", ".7z", ".rar", ".deb", ".rpm"},
+    "Disk Images": {".iso", ".img", ".vmdk"},
+    "Other": {".tid", ".vcf"},
+    "Miscellaneous": set()
+}
+
+
+# Create Current folder if it doesn't exist
+current_folder = os.path.join(path, "Current")
+if not os.path.exists(current_folder):
+    os.makedirs(current_folder)
+
+# Create folders for the extensions
+for folder_name, extensions in folders.items():
+    folder_path = os.path.join(path, folder_name)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+current_threshold = timedelta(days=90)
+for filename in os.listdir(path):
+    filepath = os.path.join(path, filename)
+    if os.path.isfile(filepath):
+        ext = os.path.splitext(filename)[1]
+        if ext in folders["Current"]:
+            shutil.move(filepath, os.path.join(current_folder, filename))
+        elif ext in folders["Miscellaneous"]:
+            shutil.move(filepath, os.path.join(path, "Miscellaneous", filename))
+        else:
+            found = False
+            for folder_name, extensions in folders.items():
+                if folder_name == "Current" or folder_name == "Miscellaneous":
+                    continue
+                if ext in extensions:
+                    folder_path = os.path.join(path, folder_name)
+                    shutil.move(filepath, os.path.join(folder_path, filename))
+                    found = True
+                    break
+            if not found:
+                file_modification_time = datetime.fromtimestamp(os.path.getmtime(filepath))
+                if datetime.now() - file_modification_time < current_threshold:
+                    shutil.move(filepath, os.path.join(current_folder, filename))
