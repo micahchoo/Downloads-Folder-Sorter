ecb6c78ee10cc84155ce489ef2e5e93a0b649849
Micah Alex <47268348+micahchoo@users.noreply.github.com>
2023-03-20 19:36:04 -0700

    monkey brain excited nowww <yawn> but its past midnight
    i learnt that the folder dictionary that I edited for this one is called a dictionary
    folders = {
    "Current": set(),
    "Images": {".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".eps", ".raw", ".cr2", ".nef", ".orf", ".sr2", ".webp", ".svg"},
    "Text Files": {".txt", ".doc", ".docx", ".log", ".md", ".opml"},
    "Presentation": {".ppt", ".pptx"},
    "Data Files": {".csv", ".key", ".keychain", ".dat", ".sdf", ".tar", ".xml", ".vcf", ".json", ".bib", ".tid"},
    "Music": {".aif", ".iff", ".m3u", ".m4a", ".mid", ".mp3", ".mpa", ".wav", ".wma"},
    "Videos": {".3g2", ".3gp", ".asf", ".avi", ".flv", ".m4v", ".mov", ".mp4", ".mpg", ".rm"},
    "Compressed Files": {".zip", ".rar", ".7z", ".bz2", ".gz", ".tar.gz"},
    "Disk Image Files": {".dmg", ".iso"},
    "Executables": {".exe", ".jar", ".msi", ".deb"},
    "System Files": {".dll", ".sys", ".cfg", ".ini", ".plist", ".folder", ".flatpakref", ".xpi"},
    "Database": {".kdbx"},
    "Misc Files": {".bak", ".tmp", ".crdownload", ".part", ""},
    "Android App Package": {".apk"},
    "Web Files": {".html", ".css", ".js"}
    }
    
    I also run this once and notice the leftover files in my downloads folder
    
    like this .kbdx file, which is the database file for an open-source password manager that is offline. i tried a setup with that and synching for a while, but it was too clunky and was causing a lot of errors.

--- Sort Files in Folders.py
+++ Sort Files in Folders.py
@@ -3,39 +3,61 @@ import shutil
 from datetime import datetime, timedelta
 
 # Define the path to the directory to organize
-dir_path = "C:/Users/micah/Downloads/_Seminar 03 - Documentation book/"
+path = "C:/Users/micah/Downloads/_Seminar 03 - Documentation book/"
 
 # Define the names of the folders to create and their associated file extensions
 folders = {
-    "Current": set(),
-    "Images": {".jpg", ".jpeg", ".png", ".gif", ".bmp"},
-    "Documents": {".pdf", ".doc", ".docx", ".txt", ".rtf", ".odt"},
-    "Videos": {".mp4", ".avi", ".mov", ".wmv", ".flv", ".mkv"},
-    "Music": {".mp3", ".wav", ".flac", ".aac", ".m4a"},
-    "Archives": {".zip", ".rar", ".tar", ".gz"},
-    "Miscellaneous": set()
+"Current": set(),
+"Images": {".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".eps", ".raw", ".cr2", ".nef", ".orf", ".sr2", ".webp", ".svg"},
+"Text Files": {".txt", ".doc", ".docx", ".log", ".md", ".opml"},
+"Presentation": {".ppt", ".pptx"},
+"Data Files": {".csv", ".key", ".keychain", ".dat", ".sdf", ".tar", ".xml", ".vcf", ".json", ".bib", ".tid"},
+"Music": {".aif", ".iff", ".m3u", ".m4a", ".mid", ".mp3", ".mpa", ".wav", ".wma"},
+"Videos": {".3g2", ".3gp", ".asf", ".avi", ".flv", ".m4v", ".mov", ".mp4", ".mpg", ".rm"},
+"Compressed Files": {".zip", ".rar", ".7z", ".bz2", ".gz", ".tar.gz"},
+"Disk Image Files": {".dmg", ".iso"},
+"Executables": {".exe", ".jar", ".msi", ".deb"},
+"System Files": {".dll", ".sys", ".cfg", ".ini", ".plist", ".folder", ".flatpakref", ".xpi"},
+"Database": {".kdbx"},
+"Misc Files": {".bak", ".tmp", ".crdownload", ".part", ""},
+"Android App Package": {".apk"},
+"Web Files": {".html", ".css", ".js"}
 }
 
+# Create Current folder if it doesn't exist
+current_folder = os.path.join(path, "Current")
+if not os.path.exists(current_folder):
+    os.makedirs(current_folder)
+
+# Create folders for the extensions
+for folder_name, extensions in folders.items():
+    folder_path = os.path.join(path, folder_name)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
 # Define the time threshold for moving files to the Current folder
-current_threshold = datetime.now() - timedelta(days=90)
+current_threshold = timedelta(days=90)
 
 # Organize the files
-for filename in os.listdir(dir_path):
-    filepath = os.path.join(dir_path, filename)
+for filename in os.listdir(path):
+    filepath = os.path.join(path, filename)
     if os.path.isfile(filepath):
-        ext = os.path.splitext(filename)[1].lower()
+        ext = os.path.splitext(filename)[1]
         if ext in folders["Current"]:
-            shutil.move(filepath, os.path.join(dir_path, "Current", os.path.basename(filename)))
+            shutil.move(filepath, os.path.join(current_folder, filename))
         elif ext in folders["Miscellaneous"]:
-            shutil.move(filepath, os.path.join(dir_path, "Miscellaneous", os.path.basename(filename)))
+            shutil.move(filepath, os.path.join(path, "Miscellaneous", filename))
         else:
             found = False
-            for folder, exts in folders.items():
-                if folder == "Current" or folder == "Miscellaneous":
+            for folder_name, extensions in folders.items():
+                if folder_name == "Current" or folder_name == "Miscellaneous":
                     continue
-                if ext in exts:
-                    shutil.move(filepath, os.path.join(dir_path, folder, os.path.basename(filename)))
+                if ext in extensions:
+                    folder_path = os.path.join(path, folder_name)
+                    shutil.move(filepath, os.path.join(folder_path, filename))
                     found = True
                     break
-            if not found and os.path.getctime(filepath) >= current_threshold:
-                shutil.move(filepath, os.path.join(dir_path, "Current", os.path.basename(filename)))
+            if not found:
+                file_creation_time = datetime.fromtimestamp(os.path.getctime(filepath))
+                if file_creation_time + current_threshold < datetime.now():
+                    shutil.move(filepath, os.path.join(current_folder, filename))
